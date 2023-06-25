85932153305a6acc9b0d5256ecc7f1967d5d94d8
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2023-06-25 18:57:46 -0300

    create: Added main's body and functions declarations

--- src/bikini_bottom.h
+++ src/bikini_bottom.h
@@ -1,2 +1,8 @@
 #include <stdio.h>
+#include <math.h>
 #include <GL/glut.h>
+
+void init(void);
+void display(void);
+void reshape (int w, int h);
+void drawEllipsoid(unsigned int uiStacks, unsigned int uiSlices, float fA, float fB, float fC);
--- src/main.c
+++ src/main.c
@@ -2,6 +2,17 @@
 
 int main(int argc, char **argv) {
 
-    printf("Bikini Bottom!\n");
+    glutInit(&argc, argv);
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_DEPTH | GLUT_RGB);
+
+    glutInitWindowSize (1280, 720);
+    glutInitWindowPosition (10, 10);
+    glutCreateWindow(argv[0]);
+
+    init();
+    glutDisplayFunc(display);
+    glutReshapeFunc(reshape);
+
+    glutMainLoop();
     return 0;
 }
